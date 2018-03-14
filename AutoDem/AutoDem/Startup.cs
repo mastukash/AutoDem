85a9b7209f9a433b47b3cfa6fa2a42c50bf03a19
mastukash <mastykash@itstep.org>
2018-03-14 15:30:56 +0200

    добавлено DAL і MVC 5

---
+++ AutoDem/AutoDem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AutoDem.Startup))]
+namespace AutoDem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
