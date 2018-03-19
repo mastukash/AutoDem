9e225433fe7a4bb02dec5dddf5eeb6855be9e34b
mastukash <mastykash@itstep.org>
2018-03-19 16:53:44 +0200

    деякі зміни

--- AutoDem/AutoDem/Controllers/HomeController.cs
+++ AutoDem/AutoDem/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using AutoDem.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,6 +11,7 @@ namespace AutoDem.Controllers
 {
     public class HomeController : Controller
     {
+        GenericUnitOfWork db = new GenericUnitOfWork();
         public ActionResult Index()
         {
             //GenericUnitOfWork repository = new GenericUnitOfWork();
@@ -26,11 +28,37 @@ namespace AutoDem.Controllers
             return View();
         }
 
+        [HttpGet]
         public ActionResult Contact()
         {
             ViewBag.Message = "Зв'яжіться із нами.";
 
             return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Contact(ContactViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            MailMessage m = new MailMessage()
+            {
+                Subject = model.Subject,
+                Body = model.Body,
+                AuthorFName = model.AuthorFName,
+                AuthorLName = model.AuthorLName,
+                Email = model.Email,
+                Phone = model.Phone,
+                Read = false,
+                DateTime = DateTime.Now
+            };
+
+            await db.Repository<MailMessage>().AddAsync(m);
+            await db.SaveAsync();
+
+            return Json(new { success = true,  message = "Повідомлення успішно надіслано" }, JsonRequestBehavior.AllowGet);
         }
     }
 }
